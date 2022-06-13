254c6c094b467913e3e46b20082f67838a76980a
Joseph Downs <jos.w.downs@gmail.com>
2022-06-13 11:50:05 -0600

    Add module

--- app/services/backgrounds_service.rb
+++ app/services/backgrounds_service.rb
@@ -1,16 +1,13 @@
 class BackgroundsService
+  include Parseable
+
   def conn
     Faraday.new(url: "https://api.unsplash.com/search/") do |f|
       f.params['client_id'] = ENV['unsplash_api_key']
     end
   end
 
-  def parse_json(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
   def get_data(location)
-    response = conn.get("photos?query=#{location}")
-    parse_json(response)
+    response = parse_json(conn.get("photos?query=#{location}"))
   end
 end
