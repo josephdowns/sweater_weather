a622f3d34617d8d9916162f397ec776a5979e058
Joseph Downs <jos.w.downs@gmail.com>
2022-06-12 23:08:44 -0600

    Add image api

--- spec/requests/api/v1/weather/image_request_spec.rb
+++ spec/requests/api/v1/weather/image_request_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe "the background image request" do
     get '/api/v1/backgrounds?location=denver,co'
 
     expect(response).to be_successful
-    image = JSON.parse(response.body, symbolize_name: true)
-    binding.pry
+    image = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(image).to have_key(:id)
+    expect(image).to have_key(:type)
+    expect(image[:type]).to eq('image')
+    expect(image).to have_key(:attributes)
+    expect(image[:attributes]).to have_key(:image_url)
+    expect(image[:attributes]).to have_key(:description)
+    expect(image[:attributes]).to have_key(:credit)
   end
 end
